81d1ffe2005184aa24414eb59bb91e8db7ea7033
tamerlanishmaev <tamerlanishmaev@gmail.com>
2023-12-24 01:10:20 +0500

    init commit

---
+++ src/main/java/com/javaintensive/telegrambot/TelegrambotApplication.java
@@ -0,0 +1,15 @@
+package com.javaintensive.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class TelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegrambotApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/javaintensive/telegrambot/constants/Buttons.java
@@ -0,0 +1,7 @@
+package com.javaintensive.telegrambot.constants;
+
+public class Buttons {
+    public final static String BUTTON_REGISTRATION = "Регистрация";
+    public final static String BUTTON_REGISTRATION_CALLBACK_TEXT = "button_reg_clicked";
+
+}
